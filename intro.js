490b337a5eccbc89f6646210427ceb731431ce0f
Tafhimbn <tafhim.aiub@gmail.com>
2024-05-19 20:22:08 +0600

    intro

---
+++ intro.js
@@ -0,0 +1,12 @@
+// Javascript code can be written inside "script" HTML tag or .js file 
+       
+       /*  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+           Impedit accusantium enim illo blanditiis quo ratione, 
+           molestias odit quaerat quae dolores nostrum ipsamculpa 
+           minima deserunt incidunt excepturi eveniet, repellendus
+            corrupti?   
+        */
+       
+       
+       document.write("My First Javascript Code")
+

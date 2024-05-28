9710c6ff9dda3e5bd3da9b7b606e444caa87c504
Tafhimbn <tafhim.aiub@gmail.com>
2024-05-28 23:42:36 +0600

    DOM

--- dom.js
+++ dom.js
@@ -8,4 +8,19 @@
 //         </body>
 
 
+/*   DOM -> Document Object Model
+-> when a web page is loaded, the browser creates a DOM of the page
+window -> document -> html -> head -> meta
+                           |       |-> meta
+                           |       |-> title
+                           |       |-> link
+                           |-> body -> div -> img
+                                    |      |-> h1
+                                    |      |-> p
+                                    |      |-> div 
+                                     -> script
+
+*/
+
+/* DOM Manipulation
 

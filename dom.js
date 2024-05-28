74d215084964b01e80fb50262c5a205495431fe5
Tafhimbn <tafhim.aiub@gmail.com>
2024-05-28 23:42:36 +0600

    dom

--- dom.js
+++ dom.js
@@ -8,4 +8,22 @@
 //         </body>
 
 
+/*   DOM -> Document Object Model
+->  Document Object Model (DOM) is an application programming interface (API) for manipulating HTML documents
+    when a web page is loaded, the browser creates a DOM of the page
+ 
+    window -> document -> html -> head -> meta
+                           |       |-> meta
+                           |       |-> title
+                           |       |-> link
+                           |-> body -> div -> img
+                                    |      |-> h1
+                                    |      |-> p
+                                    |      |-> div 
+                                     -> script
 
+*/
+
+/* DOM Manipulation */
+
+console.log(document)

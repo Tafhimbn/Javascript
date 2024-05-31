4fa3e8ff47b16911b8e6d30e72acdf6f0264daaf
Tafhimbn <tafhim.aiub@gmail.com>
2024-05-31 12:06:07 +0600

    DOM

--- dom.js
+++ dom.js
@@ -9,6 +9,9 @@
 
 
 /*   DOM -> Document Object Model
+               |       |       |-> HTML tree structure is the model
+               |       |-----> HTML elements are Object
+               |----> HTML page is a Document 
 ->  Document Object Model (DOM) is an application programming interface (API) for manipulating HTML documents
     when a web page is loaded, the browser creates a DOM of the page
  
@@ -26,4 +29,32 @@
 
 /* DOM Manipulation */
 
-console.log(document)
+console.dir(typeof(document));
+console.dir(document.title);
+document.title = "Play with DOM";
+console.dir(document.title);
+console.log(document.head);
+console.log(document.all); // print all elements of html page
+
+
+/*  Methods of Document Object Model
+
+1. write("string") -> writes the given string on the doucment.
+2. writeln("string") -> writes the given string on the doucment with newline character at the end.
+3. getElementById() -> returns the element having the given id value.
+4. getElementsByName() -> returns all the elements having the given name value.
+5. getElementsByTagName() -> returns all the elements having the given tag name.
+6. getElementsByClassName() -> returns all the elements having the given class name.
+
+*/
+
+// getElementById()
+console.log(document.getElementById("str"));
+let txt = document.getElementById("str");
+
+txt.textContent = "change element by getElementById() method "
+txt.innerText = "getElementById() method "
+
+
+
+
--- intro.js
+++ intro.js
@@ -8,7 +8,7 @@
         */
        
 
-document.write("Javascript code using document.write()")       /* Write Javascript code using document.write() */
+document.write("Javascript code using document.write()")       // Write Javascript code using document.write() //
 window.alert(" Write Javascript code using window.alert() ")  /* Using window.alert() */
 console.log("Write Javascript code using console.log()");      /* Write Javascript code using console.log(): print in console only*/
 

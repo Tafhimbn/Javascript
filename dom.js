74f326e99e473e2171653a4f673161e903625776
Tafhimbn <tafhim.aiub@gmail.com>
2024-05-31 23:17:17 +0600

    dom update ..

--- dom.js
+++ dom.js
@@ -52,10 +52,58 @@ console.log(document.all); // print all elements of html page
 console.log(document.getElementById("str"));
 let txt = document.getElementById("str");
 
-txt.textContent = "change element by getElementById() method "
-txt.innerText = "getElementById() method "
+txt.textContent = "change element by getElementById() method ";
+txt.innerText = "getElementById() method ";
+
+console.log(txt.textContent); // write html file text format
+console.log(txt.innerText);  // write element text format
+console.log(txt.innerHTML); // write html file text format
+
+txt.style.color="red"; // element style change 
+
+// getElementsByClassName()
+
+console.log(document.getElementsByClassName("item"));
+let item = document.getElementsByClassName("item");
+for(let i=0; i<item.length;i++){
+    item[i].style.color = 'green';
+}
+
+let itemUL = document.getElementById("items");
+console.log(itemUL);
+let items = itemUL.getElementsByClassName("item");
+console.log(items);
+for(let i=0; i<items.length-1;i++){
+    items[i].style.color = 'red';
+}
+
+// getElementsByTag()
+
+let tg = document.getElementsByTagName("i");
+console.log(tg);
+
+
+// Query Selector   --- use instead of Jquery 
+let qs = document.querySelector('.cls');
+console.log(qs);
 
 
 
-/* DOM Manipulation */
+let it = document.querySelector('.cls:last-child');
+it.style.color = "yellow";
+
+let its = document.querySelectorAll('.cls:last-child');
+console.log(its);
+
+let it2 = document.querySelectorAll('.cls:nth-child(2)');
+console.log(it2);
+
+for (let i of its){
+    i.style.color="pink";
+}
+
+
+// Parent/child relation
+
+
 

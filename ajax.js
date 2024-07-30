1ed1daf93ecf188a40badd7554951ee2e7a4ad66
Tafhimbn <tafhim.aiub@gmail.com>
2024-07-30 23:44:09 +0600

    AJAX Example

--- ajax.js
+++ ajax.js
@@ -170,3 +170,85 @@ function loadData3(){
   xhr.send();
 
 }
+
+
+
+function sendRequest(method, url, data) {
+  
+  const promise = new Promise ((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+      resolve(this.response);
+    };
+  
+    xhr.open(method, url);
+
+    xhr.responseType = "json";
+
+    xhr.send(data);
+});
+
+return promise;
+
+}
+
+
+function getData() {
+
+  sendRequest("GET","https://jsonplaceholder.typicode.com/todos/1").then(responseData => {
+    console.log(responseData);
+
+  });
+
+}
+
+
+function sendData() {
+  sendRequest("POST","https://jsonplaceholder.typicode.com/posts", JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  })
+  ).then((responseData) => {
+    console.log(responseData);
+
+  });
+}
+
+
+
+const getButton = document.getElementById("get");
+const sendButton = document.getElementById("send");
+
+getButton.addEventListener("click", getData);
+sendButton.addEventListener("click", sendData);
+
+
+
+
+
+
+
+/* if you want to call all function individually  
+
+Function getData() {
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.onload = function () {
+    console.log(this.responseText); // JSON String
+    console.log(JSON.parse(this.responseText)); // Object
+    console.log(JSON.parse(this.responseText).userId);
+
+   // console.log(this.response);  // when used xhr.responseType = "json"
+  };
+
+  xhr.open("GET","https://jsonplaceholder.typicode.com/todos/1");
+  
+  // xhr.responseType = "json";
+
+  xhr.send();
+}
+
+*/

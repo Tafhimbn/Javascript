36ac91946bc6c5f739303cf436086788f2932b8f
Tafhimbn <tafhim.aiub@gmail.com>
2024-08-02 22:45:20 +0600

    es6.

--- arrow_function.js
+++ arrow_function.js
@@ -1,2 +1,25 @@
 // ES6 fat arrow function
+/*
+    Arrow functions allow us to write shorter function syntax:
 
+                            let myFunction = (a, b) => a * b;
+
+        Arrow Functions Return Value by Default:
+                            let hello = () => "Hello World!";           
+
+*/
+
+
+
+let age = () => {
+    return 10;
+}
+
+let name = () => "Tafhim"
+
+console.log(name());
+console.log(age());
+
+let city = () => console.log("Chittagong");
+
+city();

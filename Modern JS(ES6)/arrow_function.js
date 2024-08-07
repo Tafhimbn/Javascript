850700691ae085a72a98067ec8ab1468ed84e4f2
Tafhimbn <tafhim.aiub@gmail.com>
2024-08-07 21:00:39 +0600

    ES6 1

--- Modern JS(ES6)/arrow_function.js
+++ Modern JS(ES6)/arrow_function.js
@@ -9,12 +9,13 @@
 
 */
 
-
+// With Arrow Function:
 
 let age = () => {
     return 10;
 }
 
+// Arrow Functions Return Value by Default:
 let name = () => "Tafhim"
 
 console.log(name());
@@ -23,3 +24,15 @@ console.log(age());
 let city = () => console.log("Chittagong");
 
 city();
+
+
+// Arrow Function With Parameters:
+
+let c_area = r => 2*r*3.14159;
+console.log(c_area(10)+" sq. m");
+
+
+let r_area = (l,w) => l*w;
+console.log(r_area(10,10)+" sq. m");
+
+

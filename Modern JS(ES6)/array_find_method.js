0c212ba82081242ae5ba02d1572d69fd2513f494
Tafhimbn <tafhim.aiub@gmail.com>
2024-09-16 13:36:55 +0600

    update

--- Modern JS(ES6)/array_find_method.js
+++ Modern JS(ES6)/array_find_method.js
@@ -8,7 +8,7 @@ Parameters:
     function(currentValue, index, arr): A function to execute on each value in the array until the first element satisfying the condition is found. 
                                         It takes three parameters:
        
-        currentValue     : The current element being processed in the array.
+        currentValue    : The current element being processed in the array.
         index (optional): The index of the current element being processed in the array.
         arr (optional)  : The array find() was called upon.
     
@@ -20,28 +20,32 @@ let num_array = [-10, -0.20, 0.30, -40, -50];
 
 // Method (return element > 0)
 let found = num_array.find(function (currentValue){
-    return currentValue > 0;
+    return currentValue;
 });
 
 // Printing desired values.
 console.log(found);
 
 
-
-
-
-/*
-
-
-var count = () => console.log("Counting...");   // Arrow Functions
-
-
-var result = numbers.find(function (currentValue, currentIndex, obj){
-    count();
-    return currentValue > 4;
+// example 2: find higher value
+let num_array1 = [2, 7, 8, 9];
+let found1 = num_array1.find(function (element) {   // Provided testing method (return element > 4).
+    return element > 5;
 });
-
-console.log(result);
-*/
+console.log(found1);                                // Printing desired values.
 
 
+
+// example 2: find even value
+function isEven(element) {
+    return element % 2 == 0;
+  }
+  
+  let randomArray = [1, 45, 8, 98, 7];
+  
+  let firstEven = randomArray.find(isEven);
+  console.log("1st Even Value: "+ firstEven); // 8
+  
+  // using arrow operator
+  let firstOdd = randomArray.find((element) => element % 2 == 1);
+  console.log("1st Odd Value: "+ firstOdd); // 1

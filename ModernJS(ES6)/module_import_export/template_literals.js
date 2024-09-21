e55e824857baf325ee87c0c6275dc81c965fdf56
Tafhimbn <tafhim.aiub@gmail.com>
2024-09-21 21:04:12 +0600

    template literals

---
+++ ModernJS(ES6)/module_import_export/template_literals.js
@@ -0,0 +1,21 @@
+// template literals
+/*
+Template Literal in ES6 provides new features to create a string that gives more control over dynamic strings. 
+Traditionally, String is created using single quotes (‘) or double quotes (“) quotes. 
+Template literal is created using the backtick (`) character.
+
+Syntax:
+          let s=`some string`;
+*/
+
+var name = "Tafhim";
+var age =25;
+
+console.log(`Hello, I am ${name}. I am ${age} years old.`);
+console.log(`10 + 71 = ${10+71}`);
+
+
+console.log(`orem Ipsum is simply dummy text of the printing and typesetting industry. 
+    
+    Lorem Ipsum has been the industry's standard                    dummy text ever since the 1500s,`);
+

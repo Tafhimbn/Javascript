f374b0014228dcf00ccc3d574325a756228d53f3
Tafhimbn <tafhim.aiub@gmail.com>
2024-09-21 20:46:45 +0600

    module import

--- ModernJS(ES6)/module_import_export/external_module.js
+++ ModernJS(ES6)/module_import_export/external_module.js
@@ -1,2 +1,18 @@
 export const pi =3.14;
 export const a = 2.9;
+
+const b = 5.7;
+export default b; // declare default export variable 
+
+// function export
+
+export function my_function() {
+    console.log("This Function import from external module.")
+}
+
+/*
+export default function my_func() {
+    console.log("This Function import from external module as default.")  // a module can't have multiple default export
+}
+
+*/
--- ModernJS(ES6)/module_import_export/module_import_export.js
+++ ModernJS(ES6)/module_import_export/module_import_export.js
@@ -1,6 +1,17 @@
 // module import / export
 
-import {pi, a} from "./external_module.js"; // named import 
+import external, {pi, a, my_function} from "./external_module.js"; // unnamed import  .... external for import function
 
+import {a as varA, pi as varPi} from "./external_module.js";  // named import
+
+import {} from "./external_module.js";  // default import
 
 console.log(pi,a);
+console.log("varA :"+varA, "varPi :"+varPi);
+console.log("Default Value: " +{});
+
+my_function();
+
+/*
+external(); // for import default function
+*/

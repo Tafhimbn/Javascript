ba5f84171fb8f634e03f662c74231af34b9faa85
Tafhimbn <tafhim.aiub@gmail.com>
2024-09-22 18:58:17 +0600

    sets weak-set

---
+++ ModernJS(ES6)/module_import_export/Sets_WeakSet.js
@@ -0,0 +1,7 @@
+// Set
+/*
+The JavaScript ES6 has introduced two new data structures, i.e Set and WeakSet.
+Set is similar to an array that allows us to store multiple items like numbers, strings, objects, etc. However, unlike an array, a set cannot contain duplicate values.
+*/
+
+let myArray = new Array();
